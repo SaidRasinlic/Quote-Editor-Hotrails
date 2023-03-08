df218ad3e78f9056eabbcaaae3cf1437afe9d06d
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-03-08 16:02:34 +0100

    Fix linter issues

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,5 @@
 class QuotesController < ApplicationController
-  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+  before_action :set_quote, only: %i[show edit update destroy]
 
   def index
     @quotes = Quote.all
@@ -15,18 +15,17 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, success: "Quote was successfully created."
+      redirect_to quotes_path, success: 'Quote was successfully created.'
     else
       render :new
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, success: "Quote was successfully updated."
+      redirect_to quotes_path, success: 'Quote was successfully updated.'
     else
       render :edit
     end
@@ -34,7 +33,7 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, success: "Quote was successfully destroyed."
+    redirect_to quotes_path, success: 'Quote was successfully destroyed.'
   end
 
   private
--- spec/features/quotes_index_spec.rb
+++ spec/features/quotes_index_spec.rb
@@ -1,56 +1,56 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "quotes/index", type: :feature do
+describe 'quotes/index', type: :feature do
   fixtures :all
   before :each do
     @quote = quotes(:first) # Reference to the first fixture quote
   end
 
-  context "upon creating a new quote we" do
+  context 'upon creating a new quote we' do
     it "should be able to see a title with text \"Quotes\" > clicking \"New quote\"
      renders a page with \"New quote\" title
       > filling the name input field with \"Capybara quotes\" should redirect us to
        home page where we'll we able to see a newly created quote" do
       visit quotes_path
-      assert_selector "h1", text: "Quotes"
+      assert_selector 'h1', text: 'Quotes'
 
-      click_on "New quote"
-      assert_selector "h1", text: "New quote"
+      click_on 'New quote'
+      assert_selector 'h1', text: 'New quote'
 
-      fill_in "Name", with: "Capybara quote"
-      click_on "Create quote"
+      fill_in 'Name', with: 'Capybara quote'
+      click_on 'Create quote'
 
-      assert_selector "h1", text: "Quotes"
-      assert_text "Capybara quote"
+      assert_selector 'h1', text: 'Quotes'
+      assert_text 'Capybara quote'
     end
   end
 
-  context "upon clicking or manipulating a quote" do
+  context 'upon clicking or manipulating a quote' do
     it "should land us on a quotes page with being able to see a quote's name" do
       visit quotes_path
       click_link @quote.name
-      assert_selector "h1", text: @quote.name
+      assert_selector 'h1', text: @quote.name
     end
 
-    it "updates a quote" do
+    it 'updates a quote' do
       visit quotes_path
-      assert_selector "h1", text: "Quotes"
+      assert_selector 'h1', text: 'Quotes'
 
-      click_on "Edit", match: :first
-      assert_selector "h1", text: "Edit quote"
+      click_on 'Edit', match: :first
+      assert_selector 'h1', text: 'Edit quote'
 
-      fill_in "Name", with: "Updated quote"
-      click_on "Update quote"
+      fill_in 'Name', with: 'Updated quote'
+      click_on 'Update quote'
 
-      assert_selector "h1", text: "Quotes"
-      assert_text "Updated quote"
+      assert_selector 'h1', text: 'Quotes'
+      assert_text 'Updated quote'
     end
 
-    it "destroys a quote" do
+    it 'destroys a quote' do
       visit quotes_path
       assert_text @quote.name
 
-      click_on "Delete", match: :first
+      click_on 'Delete', match: :first
       assert_no_text @quote.name
     end
   end
