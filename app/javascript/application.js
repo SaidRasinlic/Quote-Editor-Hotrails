1e5eeddadefc6c9fbc04b3f575707d2b07c48245
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-03-13 14:01:19 +0100

    Add file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// To see how the app will behave without Hotwire, disable the Turbo Drive framework. With the following line:
+// Turbo.session.drive = false // uncomment this line if you want to disable it, by default it's ON.
