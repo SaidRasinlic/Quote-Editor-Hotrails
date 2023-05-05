3d1d7a44b66f65eef5b4eafd4532042dc769e86d
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-05-05 19:37:55 +0200

    Add file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :validatable
+         :recoverable
 
   belongs_to :company
 
   def name
-    email.split('@').first.capitalize
+    email.split("@").first.capitalize
   end
 end
