d6bdc31b8aaef4bfaa3d8e30647e84d32cbf2928
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-05-12 12:38:53 +0200

    Validate data and create helper methods

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,7 +7,7 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 
   def total_price
     line_items.sum(&:total_price)
